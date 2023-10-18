9a03b7a2958ee9547a776e56ddb43f4caf049221
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-18 14:06:34 +0800

    second counter complete

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ reset.addEventListener("click", function () {
   count = 0;
   num.innerHTML = count;
 });
+
+const decrease2 = document.getElementById("btn2-decrease");
+const reset2 = document.getElementById("btn2-reset");
+const increase2 = document.getElementById("btn2-increase");
+
+const num2 = document.querySelector(".num2");
+let counter = 100;
+decrease2.addEventListener("click", function () {
+  counter--;
+  num2.innerHTML = counter;
+});
+increase2.addEventListener("click", function () {
+  counter++;
+  num2.innerHTML = counter;
+});
+reset2.addEventListener("click", function () {
+  counter = 0;
+  num2.innerHTML = counter;
+});
